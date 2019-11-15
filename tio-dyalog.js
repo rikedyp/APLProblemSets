148fb53655bf4b5dbe85aa2fe444d49d3e784888
rikedyp <rikedyp@users.noreply.github.com>
2019-11-15 16:12:51 +0000

    test problems, basic error handling

---
+++ tio-dyalog.js
@@ -0,0 +1,139 @@
+var runURL = "https://tio.run/cgi-bin/run"
+var quitURL = "https://tio.run/cgi-bin/quit"
+var runString = ["Vlang","1","apl-dyalog","Vargs","0","F.input.tio","0","F.code.tio"]
+var runRequest;
+
+function $(selector, parent) {
+	return (parent || document).querySelector(selector);
+}
+
+function $$(selector, parent) {
+	return (parent || document).querySelectorAll(selector);
+}
+
+function submit(p) {
+  console.log("submit " + p);  
+  var p = "P" + p.slice(1,2);    
+  var sol = $("#s0").value;
+  tioRequest(sol); 
+  // 0: Failed
+  // 1: Basic pass
+  // 2: Full pass
+}
+
+function tioRequest(code){
+  runRequest = new XMLHttpRequest;
+  runRequest.open("POST", runURL, true);
+  runRequest.responseType = "arraybuffer";
+  runRequest.onreadystatechange = runRequestOnReadyState;
+  var req = deflate(codeToByteString(code));
+  runRequest.send(req);
+}
+
+function iterate(iterable, monad) {
+	if (!iterable)
+		return;
+	for (var i = 0; i < iterable.length; i++)
+		monad(iterable[i]);
+}
+
+function runRequestOnReadyState() {
+	if (runRequest.readyState != XMLHttpRequest.DONE)
+		return; 
+  console.log(runRequest.status);
+  console.log(runRequest.statusText);
+	var response = byteArrayToByteString(new Uint8Array(runRequest.response));
+	var statusCode = runRequest.status;
+	var statusText = runRequest.statusText;
+
+  // Reset requests
+	runRequest = undefined;
+  runString = ["Vlang","1","apl-dyalog","Vargs","0","F.input.tio","0","F.code.tio"]
+
+	if (statusCode >= 400) {
+		sendMessage("Error " + statusCode, statusCode < 500 ? response || statusText : statusText);
+		return;
+	}
+
+	try {
+		var rawOutput = inflate(response.slice(10));
+	} catch(error) {
+		sendMessage("Error", "The server's response could not be decoded.");
+		return;
+	}
+
+	try {
+		response = byteStringToText(rawOutput);
+	} catch(error) {
+		response = rawOutput;
+	}
+
+	if (response.length < 32) {
+		sendMessage("Error", "Could not establish or maintain a connection with the server.");
+	}
+
+	var results = response.substr(16).split(response.substr(0, 16));
+  console.log(results[0]);
+  var errors = response.split("\n");
+  var e = errors[0].split(" ");
+  console.log(e.slice(5,7).join(" "));
+	var warnings = results.pop().split("\n");
+	var outputTextAreas = $$("#interpreter textarea.output");
+
+	iterate(warnings, function(warning) {
+		if (warning !== "")
+			sendMessage(results.toString() ? "Warning" : "Error", warning);
+	});
+
+	iterate(outputTextAreas, function(outputTextArea) {
+		outputTextArea.value = results.shift() || "";
+		resize(outputTextArea);
+	});
+}
+
+function deflate(byteString) {
+	return pako.deflateRaw(byteStringToByteArray(byteString), {"level": 9});
+}
+
+function inflate(byteString) {
+	return byteArrayToByteString(pako.inflateRaw(byteString));
+}
+
+function textToByteString(string) {
+	return unescape(encodeURIComponent(string));
+}
+
+function byteStringToText(byteString) {
+	return decodeURIComponent(escape(byteString));
+}
+
+function byteStringToByteArray(byteString) {
+	var byteArray = new Uint8Array(byteString.length);
+	for(var index = 0; index < byteString.length; index++)
+		byteArray[index] = byteString.charCodeAt(index);
+	byteArray.head = 0;
+	return byteArray;
+}
+
+function byteArrayToByteString(byteArray) {
+	var retval = "";
+	iterate(byteArray, function(byte) { retval += String.fromCharCode(byte); });
+	return retval;
+}
+
+function sendMessage(title, text) {
+	console.log(title + ":" + text);
+}
+
+function codeToByteString(code) {
+  var retval = "";
+  // runNum.forEach(function(token){
+    // retval += String.fromCodePoint(token);
+  // });
+  var value = textToByteString(code);
+  runString.push(value.length);
+  runString.push(value);
+  runString.push("R");
+  retval = runString.join("\0");     
+  return retval;
+}
