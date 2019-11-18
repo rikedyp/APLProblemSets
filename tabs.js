d159d54b5a7f6eb8e87690e7bd141148aeef7dec
rikedyp <rikedyp@users.noreply.github.com>
2019-11-18 08:45:52 +0000

    Basic styling & response

--- tabs.js
+++ tabs.js
@@ -1,6 +1,3 @@
-// window.onload = function() {
-  // setTimeout(loadTabs, 500);
-// }
 window.onload = function loadTabs() {
   console.log("hello");
   // Add language bars and inputs to problems
@@ -10,11 +7,12 @@ window.onload = function loadTabs() {
     // Add tab button 
     var btn = document.createElement("button");
     btn.classList.add("tabLink");
+    btn.id = "btn" + problem;
     btn.innerHTML = problems[problem].title;
     var p = problems[problem];
     btn.onclick = (function(opt) {
       return function() {
-        openTab(event, opt);
+        openTab(opt);
       };
     })(problem);
     tabs.appendChild(btn);   
@@ -23,14 +21,18 @@ window.onload = function loadTabs() {
     console.log(div);
     var lb = document.createElement("div");
     lb.classList.add("lb");
+    lb.classList.add("apl");    
     var input = document.createElement("input");
     input.id = "s" + problem;
+    input.classList.add("apl");
+    input.classList.add("solution");
     var submit = document.createElement("button");
     submit.onclick = (function(opt) {
       return function() {
         sub(opt);
       }
     })(problem);
+    submit.classList.add("submit");
     submit.innerHTML = "Submit";
     var feedback = document.createElement("div");
     feedback.id = "feedback" + problem;
@@ -42,15 +44,33 @@ window.onload = function loadTabs() {
   var lbScript = document.createElement("script");
   lbScript.src = "lb.js";
   document.body.appendChild(lbScript)  
+  openTab("P0");
 }
 
-function openTab(event, p) {
+function openTab(p) {
   console.log("ot");
   console.log(p);
   var i, tabs, tabLinks;
-  
+  // Get all elements with class="tab" and hide
+  tabs = document.getElementsByClassName("tab");
+  for (i = 0; i < tabs.length; i++) {
+    console.log(tabs[i]);
+    tabs[i].style.display = "none";
+  }
+  // Get all tabLinks and remove the "active" class
+  tabLinks = document.getElementsByClassName("tabLink");
+  for (i = 0; i < tabLinks.length; i++) {
+    tabLinks[i].classList.remove("active");
+  }
+  document.getElementById(p).style.display = "block";
+  // Add active class to clicked button
+  var btn = document.getElementById("btn" + p);
+  console.log(btn);
+  btn.classList.add("active");
 }
 
 function sub(p) {
   submit(p);  
 }
+
+
--- tio-dyalog.js
+++ tio-dyalog.js
@@ -4,6 +4,12 @@ var runString = ["Vlang","1","apl-dyalog","Vargs","0","F.input.tio","0","F.code.
 var testString = "{a←⎕JSON ⍺ ⋄ b e←⍎¨¨a.(a b) ⋄ +/((⍎a.f)¨¨b e)≡¨((⍎⍵)¨¨b e)}";
 var runRequest;
 var currentProblem;
+var congrats = [
+  "Congratulations! Your solution passed all of the base and edge cases",
+  "Well done! Your function was just what we needed",
+  "Awesome! Your solution couldn't be any more correct",
+  "Way to go! Your function passed all of our test cases",
+];
 
 function $(selector, parent) {
 	return (parent || document).querySelector(selector);
@@ -26,20 +32,21 @@ function submit(p) {
   // 2: Full pass
 }
 
-function outResult(result) {
+function outResult(result, error) {
   console.log(currentProblem);
   console.log(result);
+  console.log(error);
   var out = document.getElementById("feedback" + currentProblem);
   if (result == 0) {
-    out.innerHTML = "Fail";
+    out.innerHTML = "That function doesn't seem to work. " + error;
   } else if (result == 1) {
-    out.innerHTML = "Basic pass";
+    out.innerHTML = "Passed all basic cases. Try to solve with <code class='apl'>⍵ ← " + problems[currentProblem].b[0] + "</code> for full marks";
   } else if (result == 2) {
-    out.innerHTML = "Full pass";
+    out.innerHTML = goldTrophy;
+    out.innerHTML += "  " + congrats[Math.floor(Math.random() * congrats.length)];
   } else {
-    out.innerHTML = "Error";
+    out.innerHTML = error;
   }
-  //out.innerHTML = result;
 }
 
 function doubleQuotes(string){
@@ -102,11 +109,11 @@ function runRequestOnReadyState() {
   console.log("got:"+results[0]);  
   var errors = response.split("\n");
   var e = errors[0].split(" ");
-  console.log(e.slice(5,7).join(" "));
+  var error = e.slice(5,7).join(" ");
 	var warnings = results.pop().split("\n");
 	var outputTextAreas = $$("#interpreter textarea.output");  
   
-  outResult(results[0]);
+  outResult(results[0], error);
   
 	iterate(warnings, function(warning) {
 		if (warning !== "")
@@ -161,3 +168,5 @@ function codeToByteString(code) {
   runString.push("R");
   return runString.join("\0");       
 }
+
+goldTrophy = '<svg class="svg-inline--fa fa-trophy-alt fa-w-18 notsubmitted" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="trophy-alt" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" data-fa-i2svg=""><path fill="currentColor" d="M552 64H448V24c0-13.3-10.7-24-24-24H152c-13.3 0-24 10.7-24 24v40H24C10.7 64 0 74.7 0 88v56c0 66.5 77.9 131.7 171.9 142.4C203.3 338.5 240 360 240 360v72h-48c-35.3 0-64 20.7-64 56v12c0 6.6 5.4 12 12 12h296c6.6 0 12-5.4 12-12v-12c0-35.3-28.7-56-64-56h-48v-72s36.7-21.5 68.1-73.6C498.4 275.6 576 210.3 576 144V88c0-13.3-10.7-24-24-24zM64 144v-16h64.2c1 32.6 5.8 61.2 12.8 86.2-47.5-16.4-77-49.9-77-70.2zm448 0c0 20.2-29.4 53.8-77 70.2 7-25 11.8-53.6 12.8-86.2H512v16zm-127.3 4.7l-39.6 38.6 9.4 54.6c1.7 9.8-8.7 17.2-17.4 12.6l-49-25.8-49 25.8c-8.8 4.6-19.1-2.9-17.4-12.6l9.4-54.6-39.6-38.6c-7.1-6.9-3.2-19 6.7-20.5l54.8-8 24.5-49.6c4.4-8.9 17.1-8.9 21.5 0l24.5 49.6 54.8 8c9.6 1.5 13.5 13.6 6.4 20.5z"></path></svg>'
