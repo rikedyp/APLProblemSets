9bf458c8832272ab6602bb3dda8a9ec018d7c98e
rikedyp <rikedyp@users.noreply.github.com>
2020-01-30 09:39:57 +0000

    new try apl

---
+++ tryapl/script.js
@@ -0,0 +1,81 @@
+// tryapl.org
+var version = 3.0;
+var currentBook;
+var currentCell = 0;
+
+$=s=>document.querySelector(s);
+$$=s=>document.querySelectorAll(s);
+
+window.MathJax = {
+  tex: {
+    inlineMath: [["$", "$"]]
+  }
+};
+
+window.onload=_=>{
+  log("TryAPL v." + version);
+  Split(["#leftPane", "#rightPane"], {
+    sizes: [40, 60],
+  })  
+  loadTioDyalog();
+  $(".gutter").innerHTML += "<span id='full' onclick='sessionFS()'>◀</span>";
+  hi.classList.add("active");
+  hiTab.classList.add("active");
+}
+
+showTab=id=>{
+  $$(".content").forEach(fn=tab=>{    
+    if (id != tab.id) {      
+      $("#" + tab.id + "Tab").classList.remove("active");
+      tab.classList.remove("active");
+    } else {
+      tab.classList.add("active");
+      $("#" + tab.id + "Tab").classList.add("active");
+    }
+  })
+}
+
+sessionFS=_=>{
+  // Full screen session pane
+}
+
+nbnext=_=>{
+  log("next");
+  cell = currentBook.cells[currentCell]
+  log(cell);
+  switch (cell.cell_type) {
+    case "code":
+      session.value += cell.source;
+      putCursor(session.value.length);
+      submitLine();
+    default:
+      mdrender.innerHTML += marked(cell.source[0]) + " <br>";
+      MathJax.texReset();
+      MathJax.typesetClear();
+      MathJax.typeset(["#mdrender"]);
+      // mdrender.innerHTML = marked(mdrender.innerHTML);
+  }
+  currentCell += 1;
+}
+
+clickAPL=code=>{
+  log("clicked APL code");
+}
+
+nbload=id=> {
+  url = $(id).value;
+  log("running notebook @:" + url);  
+  fetch(nburl(url)).then((response)=>{
+    return response.json();
+  }).then((json)=>{   
+    currentBook = json;      
+  })
+}
+
+nburl=url=> {
+  log(url);
+  if (url.includes("github") && !url.includes("raw.githubusercontent")) {
+    log("https://raw.githubusercontent.com" + url.split("https://github.com")[1].split("/blob").join(""));
+    return "https://raw.githubusercontent.com" + url.split("https://github.com")[1].split("/blob").join("");
+  }
+}
